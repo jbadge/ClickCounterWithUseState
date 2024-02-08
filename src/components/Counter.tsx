97ae2786ff8658810f99d368e66ae6ad49d1bff1
Jeremy Badger <jeremebadger@gmail.com>
2024-02-08 15:25:27 -0800

    Added inline functions

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,17 +2,26 @@ import React, { useState } from 'react'
 
 export function Counter() {
   const [counter, setCounter] = useState(0)
+  const [name, setName] = useState('')
 
-  function handleClickCounter(event: React.MouseEvent) {
-    event.preventDefault()
+  // function handleClickCounter(event: React.MouseEvent) {
+  //   event.preventDefault()
+  //   setCounter(counter + 1)
+  // }
 
-    setCounter(counter + 1)
-  }
+  // function handleChangeInput(event: React.ChangeEvent<HTMLInputElement>) {
+  //   setName(event.target.value)
+  // }
 
   return (
     <div>
-      <p>The count is {counter}</p>
-      <button onClick={handleClickCounter}>Increment</button>
+      <p>
+        Hi, {name}! The count is {counter}
+      </p>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <p>
+        <input value={name} onChange={(event) => setName(event.target.value)} />
+      </p>
     </div>
   )
 }
